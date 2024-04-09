8bc3ce744d9ab4df8845352b6ef97e85066184ef
Chris <0355882@student.rocvantwente.nl>
2024-04-09 17:21:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using ClassLib;
+namespace _2073._Time_Needed_to_Buy_Tickets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] tickets = [84, 49, 5, 24, 70, 77, 87, 8];
+            int k = 3;
+            int result = solution.TimeRequiredToBuy(tickets, k);
+            Console.WriteLine(result);
+        }
+    }
+}
